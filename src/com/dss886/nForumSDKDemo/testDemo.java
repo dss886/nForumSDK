c91f1fb36736f92ef3fe51a6f3e0057e2efd202e
dss886 <dss886@10.8.168.27>
2014-09-13 17:12:40 +0800

    update Readme.md

--- src/com/dss886/nForumSDKDemo/testDemo.java
+++ src/com/dss886/nForumSDKDemo/testDemo.java
@@ -11,7 +11,7 @@ import com.dss886.nForumSDK.util.Constant;
 
 public class testDemo {
 	public static void main(String... args){
-		NForumSDK nSDK = new NForumSDK(Constant.HOST_BYR, "78e223c052793f0b", "dss886", "e!D3Qe!F728x");
+		NForumSDK nSDK = new NForumSDK(Constant.HOST_BYR, "78e223c052793f0b", "guest", "");
 		AttachmentService attachmentService = nSDK.getAttachmentService();
 		try {
 			attachmentService.delAttachment("Hubei", 384937, "test.txt");
